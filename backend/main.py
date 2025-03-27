29cbec521ba5ed813cb0bf3ea90ab0180a29ef86
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-27 01:45:42 -0600

    added logic to run the app

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,19 @@ from typing import Annotated, Union
 
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def home(request: Request):
+    pass
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app", 
+        host="127.0.0.1", 
+        port=8006, 
+        reload=True
+    )
